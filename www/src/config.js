8b93b771927c03f9607cf10bdd36643d24a04883
Abby Jones <abbyjonesdev@gmail.com>
2022-06-26 11:35:33 -0500

    update front end config after initial deployment

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "63o98qiana2d4bvgt9koud2uia",     // CognitoClientID
+  "api_base_url": "https://01dcqfgc69.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-todolambdarefarch.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d17wqbuvmby77n.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
